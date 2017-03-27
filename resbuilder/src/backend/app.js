a17fe9ca95ad72bd4ea1b1994c82127d20c45f17
Savan <savanpatel3@gmail.com>
2017-03-26 23:06:47 -0400

    First commit!

---
+++ resbuilder/src/backend/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by savan on 21/2/17.
+ */
+module.exports = function(app, mongooseAPI) {
+    /*require("./services/user.service.server.js")(app, mongooseAPI);
+    require("./services/website.service.server.js")(app, mongooseAPI);
+    require("./services/page.service.server.js")(app, mongooseAPI);
+    require("./services/widget.service.server.js")(app, mongooseAPI);*/
+};
---
+++ resbuilder/src/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/frontend'));
+
+//require ("./test/app.js")(app);
+//var mongooseAPI = require("./model/server")(app);
+var mongooseAPI = null;
+require("./backend/app.js")(app, mongooseAPI);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
