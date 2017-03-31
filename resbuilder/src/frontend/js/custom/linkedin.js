7bfd965ce72d0ef455c4a9739d7ec8cd1259b225
Savan <savanpatel3@gmail.com>
2017-03-31 18:45:29 -0400

    Redesigned landing page. Fixed menu drop down issue. Integrated linkedIn import for basic profile.

---
+++ resbuilder/src/frontend/js/custom/linkedin.js
@@ -0,0 +1,23 @@
+    function liAuth(){
+        IN.User.authorize(function(){
+        });
+    }
+// Setup an event listener to make an API call once auth is complete
+function onLinkedInLoad() {
+    IN.Event.on(IN, "auth", getProfileData);
+}
+
+// Handle the successful return from the API call
+function onSuccess(data) {
+    console.log(data);
+}
+
+// Handle an error response from the API call
+function onError(error) {
+    console.log(error);
+}
+
+// Use the API call wrapper to request the member's basic profile data
+function getProfileData() {
+    IN.API.Raw("/people/~:(id,first-name,last-name,positions,specialties)").result(onSuccess).error(onError);
+}
